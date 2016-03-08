584db303f353295d63c6dc28e6a0b16c23a54813
lakitrid <eric.dumortier@gmail.com>
2016-03-08 08:11:12 +0100

    Creation of the solution and initialization of the GTL part that will run on Rpi 2

---
+++ GTLNode/Services/PowerService.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Windows.Devices.Enumeration;
+using Windows.Devices.SerialCommunication;
+
+namespace GTLNode.Services
+{
+    internal class PowerService
+    {
+        public PowerService()
+        {
+            this.Init();
+        }
+
+        public AutoResetEvent Handle { get; private set; }
+
+        private async void Init()
+        {
+            this.Handle = new AutoResetEvent(false);
+
+        }
+
+        private void Run()
+        {
+
+        }
+
+        private async Task<bool> InitSerial()
+        {
+            try
+            {
+                string aqs = SerialDevice.GetDeviceSelector("UART0");
+                DeviceInformationCollection dis = await DeviceInformation.FindAllAsync(aqs);
+                SerialDevice serialPort = await SerialDevice.FromIdAsync(dis[0].Id);
+
+                serialPort.BaudRate = 1200;
+                serialPort.Parity = SerialParity.Even;
+                serialPort.DataBits = 7;
+                serialPort.StopBits = SerialStopBitCount.One;
+
+                serialPort.ReadTimeout = TimeSpan.FromSeconds(1);
+
+                return true;
+
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ GTLNode/StartupTask.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net.Http;
+using Windows.ApplicationModel.Background;
+using uPLibrary.Networking.M2Mqtt;
+using System.Net;
+using uPLibrary.Networking.M2Mqtt.Messages;
+using System.Threading;
+using GTLNode.Services;
+
+// The Background Application template is documented at http://go.microsoft.com/fwlink/?LinkID=533884&clcid=0x409
+
+namespace GTLNode
+{
+    public sealed class StartupTask : IBackgroundTask
+    {
+        private MqttClient _client;
+        private PowerService _powerService;
+
+        public void Run(IBackgroundTaskInstance taskInstance)
+        {
+            this._powerService = new PowerService();
+
+            Timer timer = new Timer(Tick, null, (int)TimeSpan.FromSeconds(5).TotalMilliseconds, (int)TimeSpan.FromSeconds(5).TotalMilliseconds);
+
+            this._client = new MqttClient("192.168.1.220");
+
+            this._client.Connect("GTLNode-Rpi-1");
+
+            AutoResetEvent handle = new AutoResetEvent(false);
+
+            handle.WaitOne();
+        }
+
+        private void Tick(object state)
+        {
+            string test = "test value";
+
+            this._client.Publish("home-power", Encoding.UTF8.GetBytes(test), MqttMsgBase.QOS_LEVEL_AT_LEAST_ONCE, true);
+        }
+    }
+}
