487db84abb21ca26946236d7d5928180b8446db6
lakitrid <eric.dumortier@gmail.com>
2016-03-13 22:15:02 +0100

    Add common for core project Start defining data model

---
+++ Common.Core/Domain/TimeSerie.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.Domain
+{
+    public class TimeSerie
+    {
+        public decimal Id { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public string Code { get; set; }
+
+        public decimal Value { get; set; }
+    }
+}
--- WebSite/Models/ApplicationDbContext.cs
+++ WebSite/Models/ApplicationDbContext.cs
@@ -4,17 +4,17 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Identity.EntityFramework;
 using Microsoft.Data.Entity;
+using Common.Domain;
 
 namespace WebSite.Models
 {
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
+        public DbSet<TimeSerie> TimeSeries { get; set; }
+
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
-            // Customize the ASP.NET Identity model and override the defaults if needed.
-            // For example, you can rename the ASP.NET Identity table names and more.
-            // Add your customizations after calling base.OnModelCreating(builder);
         }
     }
 }
